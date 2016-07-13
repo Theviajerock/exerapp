d04c794752cd81555041e505018dcdd1187b28f7
victor <victor@ilujdebian.sysdev.co>
2016-07-12 21:54:22 -0500

    Register storing in database

--- __init__.py
+++ __init__.py
@@ -10,17 +10,30 @@ def before_request():
 @app.teardown_request
 def teardown_request(exception):
     if hasattr(g, 'db'):
+        g.db.commit()
         g.db.close()
 
 @app.route('/', methods=["POST","GET"])
 def index():
-    if request.method=="POST":
-        session['email'] = request.form['email']
-        session['username'] = request.form['username']
-        session['password'] = request.form['password']
-        base = "base_logged.html"
-        username = session['username']
-        return render_template('index.html', base=base, username=username)
+    if request.method == "POST":
+        email = request.form['email']
+        username = request.form['username']
+        password = request.form['password']
+        query = g.db.execute('SELECT * FROM USERS WHERE USERNAME = ?;',[username]).fetchall()
+        if len(query) > 0:
+            base = "base_not_logged.html"
+            return render_template('index.html', base=base)
+        else:
+            session['email'] = email
+            session['username'] = username
+            session['password'] = username
+            g.db.execute('INSERT INTO USERS(USERNAME, PASSWORD, EMAIL) VALUES(?,?,?);', [username, password, email])
+            a = g.db.execute('SELECT * FROM USERS').fetchall()
+            print(a)
+            base = "base_logged.html"
+            print(base)
+            username = session['username']
+            return render_template('index.html', base=base, username=username)
     if 'username' in session:
         base = "base_logged.html"
         username = session['username']
@@ -36,19 +49,8 @@ def logout():
 
 app.secret_key = 'victorhernandez'
 
-"""
-@app.route('/register', methods=["POST","GET"])
-def register():
-    if request.method == "POST":
-        session['username'] = request.form['username']
-        session['password'] = request.form['password']
-        session['email'] = request.form['email']
-        return redirect(url_for('index'))
-    elif 'username' in session:
-        return redirect(url_for('index'))
-    else:
-        base = "base_not_logged.html"
-        return render_template('register.html', base=base)
-"""
+@app.route('/login')
+def login():
+    return "hola"
 
 app.run(debug=True)
